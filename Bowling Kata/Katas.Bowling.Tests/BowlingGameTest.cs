5654cdfaf97db5c5646acac88529b8ae978cc2de
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-03-26 18:08:17 +0200

    Added tests than shows wrong design and that can't be 'green'ed' until refactoring.

--- Bowling Kata/Katas.Bowling.Tests/BowlingGameTest.cs
+++ Bowling Kata/Katas.Bowling.Tests/BowlingGameTest.cs
@@ -27,6 +27,18 @@ namespace Katas.Bowling.Tests
             Assert.Equal(0, game.Score);
         }
 
+        //[Fact]
+        //public void OneSpare()
+        //{
+        //    game.Roll(5);
+        //    game.Roll(5); // spare
+        //    game.Roll(3);
+
+        //    RollMany(17, 0);
+
+        //    Assert.Equal(16, game.Score);
+        //}
+
         private void RollMany(int n, int pins)
         {
             for (int i = 0; i < n; i++)
