f992899d2b01e2d76b606e7e9577b350c0a68b96
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-03-26 17:14:13 +0200

    Finished 1st test that drove Game class itself, Roll method and Score property

--- Bowling Kata/Katas.Bowling.Tests/BowlingGameTest.cs
+++ Bowling Kata/Katas.Bowling.Tests/BowlingGameTest.cs
@@ -1,6 +1,20 @@
-﻿namespace Katas.Bowling.Tests
+﻿using Xunit;
+
+namespace Katas.Bowling.Tests
 {
     public class BowlingGameTest
     {
+        [Fact]
+        public void GutterGame()
+        {
+            Game g = new Game();
+
+            for (int i = 0; i < 20; i++)
+            {
+                g.Roll(0);
+            }
+
+            Assert.Equal(0, g.Score);
+        }
     }
 }
---
+++ Bowling Kata/Katas.Bowling/Game.cs
@@ -0,0 +1,11 @@
+﻿namespace Katas.Bowling
+{
+    public class Game
+    {
+        public int Score => 0;
+
+        public void Roll(int pins)
+        {
+        }
+    }
+}
