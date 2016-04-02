60a696758e7f98c625a01bcdf0734ab09dac5619
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-04-02 17:55:01 +0300

    2nd tests - All Ones - passed

--- Bowling Kata/Katas.Bowling.Tests/BowlingGameTests.cs
+++ Bowling Kata/Katas.Bowling.Tests/BowlingGameTests.cs
@@ -16,5 +16,18 @@ namespace Katas.Bowling.Tests
 
             Assert.Equal(0, game.Score());
         }
+
+        [Fact]
+        public void AllOnes()
+        {
+            Game game = new Game();
+
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(1);
+            }
+
+            Assert.Equal(20, game.Score());
+        }
     }
 }
