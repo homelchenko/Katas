61dd0bb4d98fb81d5123f4b8701cc3d9c1f391b3
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-04-02 17:51:40 +0300

    First tests - Gutter Game - passed

---
+++ Bowling Kata/Katas.Bowling.Tests/BowlingGameTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Katas.Bowling.Tests
+{
+    public class BowlingGameTests
+    {
+        [Fact]
+        public void GutterGame()
+        {
+            Game game = new Game();
+
+            for (int i = 0; i < 20; i++)
+            {
+                game.Roll(0);
+            }
+
+            Assert.Equal(0, game.Score());
+        }
+    }
+}
---
+++ Bowling Kata/Katas.Bowling/Game.cs
@@ -0,0 +1,14 @@
+﻿namespace Katas.Bowling
+{
+    public class Game
+    {
+        public void Roll(int pins)
+        {
+        }
+
+        public int Score()
+        {
+            return 0;
+        }
+    }
+}
