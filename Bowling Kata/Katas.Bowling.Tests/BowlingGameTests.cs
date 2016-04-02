b2b5e7f7d697384baa7af8693f4790e1e3d0255c
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-04-02 18:28:54 +0300

    3rd tests - Spare - passes

--- Bowling Kata/Katas.Bowling.Tests/BowlingGameTests.cs
+++ Bowling Kata/Katas.Bowling.Tests/BowlingGameTests.cs
@@ -27,12 +27,29 @@ namespace Katas.Bowling.Tests
             Assert.Equal(0, _game.Score());
         }
 
+        [Fact]
+        public void OneSpare()
+        {
+            RollSpare();
+            _game.Roll(3);
+
+            RollMany(17, 0);
+
+            Assert.Equal(16, _game.Score());
+        }
+
         private void RollMany(int n, int pins)
         {
             for (int i = 0; i < n; i++)
             {
                 _game.Roll(pins);
             }
+        }
+
+        private void RollSpare()
+        {
+            _game.Roll(5);
+            _game.Roll(5);
         }
     }
 }
--- Bowling Kata/Katas.Bowling/Game.cs
+++ Bowling Kata/Katas.Bowling/Game.cs
@@ -2,16 +2,41 @@
 {
     public class Game
     {
-        private int _score = 0;
+        private readonly int[] _rolls = new int[21];
+
+        private int _currentRoll;
 
         public void Roll(int pins)
         {
-            _score += pins;
+            _rolls[_currentRoll++] = pins;
         }
 
         public int Score()
         {
-            return _score;
+            int score = 0;
+
+            int frameIndex = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsSpare(frameIndex))
+                {
+                    score += 10 + _rolls[frameIndex + 2];
+                }
+                else
+                {
+                    score += _rolls[frameIndex] + _rolls[frameIndex + 1];
+                }
+
+                frameIndex += 2;
+            }
+
+            return score;
+        }
+
+        private bool IsSpare(int frameIndex)
+        {
+            return _rolls[frameIndex] + _rolls[frameIndex + 1] == 10;
         }
     }
 }
