5a9a6b8e41620d7b7e8f5070402ac2c0894b9a0c
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-04-09 15:35:39 +0300

    Kata finished

---
+++ Bowling Kata/Katas.Bowling.Tests/BowlingGameTests.cs
@@ -0,0 +1,81 @@
+﻿using Xunit;
+
+namespace Katas.Bowling.Tests
+{
+    public class BowlingGameTests
+    {
+        private readonly BowlingGame _game;
+
+        public BowlingGameTests()
+        {
+            _game = new BowlingGame();
+        }
+
+        [Fact]
+        public void AllOnes()
+        {
+            RollMany(20, 1);
+
+            Assert.Equal(20, _game.GetScore());
+        }
+
+        [Fact]
+        public void GutterGame()
+        {
+            RollMany(20, 0);
+
+            Assert.Equal(0, _game.GetScore());
+        }
+
+        [Fact]
+        public void OneSpare()
+        {
+            RollSpare();
+
+            _game.Roll(3);
+
+            RollMany(17, 0);
+
+            Assert.Equal(16, _game.GetScore());
+        }
+
+        [Fact]
+        public void OneStrike()
+        {
+            RollStrike();
+            _game.Roll(4);
+            _game.Roll(6);
+
+            RollMany(16, 0);
+
+            Assert.Equal(30, _game.GetScore());
+        }
+
+        [Fact]
+        public void PerfectGame()
+        {
+            RollMany(12, 10);
+
+            Assert.Equal(300, _game.GetScore());
+        }
+
+        private void RollMany(int rolls, int pinsPerRoll)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                _game.Roll(pinsPerRoll);
+            }
+        }
+
+        private void RollSpare()
+        {
+            _game.Roll(5);
+            _game.Roll(5);
+        }
+
+        private void RollStrike()
+        {
+            _game.Roll(10);
+        }
+    }
+}
---
+++ Bowling Kata/Katas.Bowling/BowlingGame.cs
@@ -0,0 +1,72 @@
+﻿namespace Katas.Bowling
+{
+    /// <summary>
+    ///     A separate Bowling game.
+    /// </summary>
+    public class BowlingGame
+    {
+        private readonly int[] _rolls = new int[21];
+
+        private int _currentRoll;
+
+        public int GetScore()
+        {
+            int score = 0;
+            int frameFirstRollIndex = 0;
+
+            for (int frameIndex = 0; frameIndex < 10; frameIndex++)
+            {
+                if (IsStrike(frameFirstRollIndex))
+                {
+                    score += 10 + GetStrikeBonus(frameFirstRollIndex);
+
+                    frameFirstRollIndex++;
+                }
+                else if (IsSpare(frameFirstRollIndex))
+                {
+                    score += 10 + GetSpareBonus(frameFirstRollIndex);
+
+                    frameFirstRollIndex += 2;
+                }
+                else
+                {
+                    score += GetFrameScore(frameFirstRollIndex);
+
+                    frameFirstRollIndex += 2;
+                }
+            }
+
+            return score;
+        }
+
+        public void Roll(int pins)
+        {
+            _rolls[_currentRoll++] = pins;
+        }
+
+        private int GetFrameScore(int frameFirstRollIndex)
+        {
+            return _rolls[frameFirstRollIndex] + _rolls[frameFirstRollIndex + 1];
+        }
+
+        private int GetSpareBonus(int afterSpareRollIndex)
+        {
+            return _rolls[afterSpareRollIndex + 2];
+        }
+
+        private int GetStrikeBonus(int strikeRollIndex)
+        {
+            return _rolls[strikeRollIndex + 1] + _rolls[strikeRollIndex + 2];
+        }
+
+        private bool IsSpare(int frameFirstRollIndex)
+        {
+            return _rolls[frameFirstRollIndex] + _rolls[frameFirstRollIndex + 1] == 10;
+        }
+
+        private bool IsStrike(int frameFirstRollIndex)
+        {
+            return _rolls[frameFirstRollIndex] == 10;
+        }
+    }
+}
