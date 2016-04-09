9a79da86a22dd2548ca6e9a52455647a8195ecaa
Hennadii Omelchenko <hennadii.omelchenko@hotmail.com>
2016-04-09 17:45:58 +0300

    Very first test passes

---
+++ Bowling Kata/Katas.Bowling.Tests/GameTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+
+namespace Katas.Bowling.Tests
+{
+    public class GameTests
+    {
+        [Fact]
+        public void Score_WhenAllRollsGiveZeroPins_ShouldBeZero()
+        {
+            // Arrange
+            Game game = new Game();
+
+            // Act
+            int gameScore = game.Score();
+            
+            // Assert
+            Assert.Equal(0, gameScore);
+        }
+    }
+}
---
+++ Bowling Kata/Katas.Bowling/GAme.cs
@@ -0,0 +1,10 @@
+﻿namespace Katas.Bowling
+{
+    public class Game
+    {
+        public int Score()
+        {
+            return 0;
+        }
+    }
+}
